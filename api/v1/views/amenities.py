37acd7e7acf77a7ff1f5240ab4468a10ad262a95
Justin4587 <Thurman@atlasok.com>
2021-05-10 15:10:05 -0500

    amenity

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -11,14 +11,14 @@ import json
                  strict_slashes=False)
 @app_views.route('/amenities/<amenitiy_id>/', methods=['GET'],
                  strict_slashes=False)
-def get_cities_de_amenidfgtie(amenity_id=None):
+def get_cities_de_amenity(amenity_id=None):
     """list of all cities in a state"""
     the_states = storage.all('Amenity')
     cities_list = []
-    if state_id is None or amenity_id is "":
+    if amenity_id is None or amenity_id is "":
         for i in the_states:
             cities_list.append(i.to_dict())
-        return jsonify(state_list)
+        return jsonify(cities_list)
     for the_one in the_states:
         if the_one.id == amenity_id:
             return jsonify(the_one.to_dict())
