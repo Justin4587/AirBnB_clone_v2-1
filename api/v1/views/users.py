ab97636742cbee642478222d0af7bbe501d99944
Corbin <forstupidityonly@gmail.com>
2021-05-11 10:21:45 -0500

    user like state

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -7,14 +7,18 @@ from models.user import User
 import json
 
 
-@app_views.route('/users/<user_id>', methods=['GET'],
-                 strict_slashes=False)
-def get_user(user_id=None):
-    """list of all users"""
-    the_users = storage.all('user').values()
-    users_list = []
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def get_a_user(user_id=None):
+    """ get stuff """
+    the_users = storage.all('User').values()
+    user_list = []
+    if user_id is None or user_id is "":
+        for i in the_users:
+            state_list.append(i.to_dict())
+        return jsonify(user_list)
     for the_one in the_users:
         if the_one.id == user_id:
             return jsonify(the_one.to_dict())
-    abort(404)
-
+        else:
+            abort(404)
