923db9b8eabac9d1ad7d47044098a715d3342dd8
Justin4587 <Thurman@atlasok.com>
2021-05-11 15:57:43 -0500

    task 15

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -44,7 +44,12 @@ class FileStorage:
         """serializes __objects to the JSON file (path: __file_path)"""
         json_objects = {}
         for key in self.__objects:
-            json_objects[key] = self.__objects[key].to_dict()
+            if self.__objects[key].__class__.__name__ == "User":
+                temp_pass = self.__objects[key].to_dict()
+                temp_pass["password"] = self.__objects[key].password
+                json_objects[key] = temp_pass
+            else:
+                json_objects[key] = self.__objects[key].to_dict()
         with open(self.__file_path, 'w') as f:
             json.dump(json_objects, f)
 
