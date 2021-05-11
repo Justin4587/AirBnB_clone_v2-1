ac90452fd0a72263830047b672a315f7313c7805
Justin4587 <Thurman@atlasok.com>
2021-05-11 10:25:28 -0500

    updated amenitititites with ben and vals help

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -7,7 +7,6 @@ from models import storage
 import json
 
 
-
 @app_views.route('/amenities/', methods=['GET'],
                  strict_slashes=False)
 @app_views.route('/amenities/<amenity_id>', methods=['GET'],
@@ -60,15 +59,15 @@ def update_a_amen(amenity_id=None):
     """replace existing data at the specified resource"""
     all_as = storage.all(Amenity).values()
     get_a = request.get_json()
-    
+
     if get_a is None:
         abort(400, {'Not a JSON'})
 
-    for the_one in all_as.values():
+    for the_one in all_as:
         if the_one.id == amenity_id:
             for key, value in get_a.items():
                 setattr(the_one, key, value)
             my_amen = Amenity(**get_a)
-            my_amen.save()  
+            my_amen.save()
             return (jsonify(the_one.to_dict()), 200)
     abort(404)
