453bcc62e29e7f0ce3545a9c91ae0fdb0843749e
Corbin <forstupidityonly@gmail.com>
2021-05-11 13:16:59 -0500

    flipyty flop the get

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -11,14 +11,13 @@ import json
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def get_a_user(user_id=None):
     """ get stuff """
+    if user_id is None:
+        users = [user.to_dict() for user in storage.all("User").values()]
+        return jsonify(users)
     the_users = storage.all('User').values()
-    user_list = []
-    if user_id is None or user_id is "":
-        for i in the_users:
-            state_list.append(i.to_dict())
-        return jsonify(user_list)
     for the_one in the_users:
         if the_one.id == user_id:
-            return jsonify(the_one.to_dict())
+            return jsonify(the_one.todict())
         else:
             abort(404)
+
